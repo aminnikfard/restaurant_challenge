d5c22c344ed4649df2719bfeb3e9800523801126
aminnikfard <aminnikfard110@gmail.com>
2021-06-14 14:56:33 +0430

    stepper for create challenge

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:restaurant_challenge_app/screens/register_phone_screen.dart';
 
+import 'date_picker/date_picker_widget.dart';
+import 'date_picker/time_picker_widget.dart';
+
 class HomeScreen extends StatefulWidget {
   static String id = 'home_screen';
 
@@ -11,14 +14,77 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final _auth = FirebaseAuth.instance;
+  int _currentStep = 0;
+
+  StepperType stepperType = StepperType.vertical;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HomeScreen'),
+        automaticallyImplyLeading: false,
+        title: Text('Flutter Stepper Demo'),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: Stepper(
+                type: stepperType,
+                physics: ScrollPhysics(),
+                currentStep: _currentStep,
+                onStepTapped: (step) => tapped(step),
+                onStepContinue: continued,
+                onStepCancel: cancel,
+                steps: <Step>[
+                  Step(
+                    title: new Text('Date You Want To Go To The Restaurant'),
+                    content: Column(
+                      children: <Widget>[
+                        DatePickerWidget(),
+                      ],
+                    ),
+                    isActive: _currentStep >= 0,
+                    state: _currentStep >= 0
+                        ? StepState.complete
+                        : StepState.disabled,
+                  ),
+                  Step(
+                    title: new Text('Time You Want To Go To The Restaurant'),
+                    content: Column(
+                      children: <Widget>[
+                        TimePickerWidget(),
+                      ],
+                    ),
+                    isActive: _currentStep >= 0,
+                    state: _currentStep >= 1
+                        ? StepState.complete
+                        : StepState.disabled,
+                  ),
+                  Step(
+                    title: new Text('Zip Code City Number'),
+                    content: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          decoration:
+                              InputDecoration(labelText: 'Zip Code City Number'),
+                        ),
+                      ],
+                    ),
+                    isActive: _currentStep >= 0,
+                    state: _currentStep >= 2
+                        ? StepState.complete
+                        : StepState.disabled,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.logout),
         onPressed: () async {
           await _auth.signOut();
           Navigator.pushReplacement(
@@ -28,8 +94,22 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           );
         },
-        child: Icon(Icons.logout),
       ),
     );
+  }
+
+  tapped(int step) {
+    setState(() => _currentStep = step);
+  }
+
+  continued() {
+    _currentStep < 2 ? setState(() => _currentStep += 1) : setState(() => null);
+    if(_currentStep >= 2) {
+      print('Hello');
+    }
+  }
+
+  cancel() {
+    _currentStep > 0 ? setState(() => _currentStep -= 1) : setState(() => null);
   }
 }
