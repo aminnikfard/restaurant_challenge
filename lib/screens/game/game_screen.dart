3c5b56054d2c2fc0b1bc73cc3dadfddaa4df237d
moh3enit <moh3enit.tmwork@gmail.com>
2021-07-19 01:44:57 +0430

    update3 by moh3en

--- lib/screens/game/game_screen.dart
+++ lib/screens/game/game_screen.dart
@@ -20,9 +20,11 @@ class _GameScreenState extends State<GameScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FeatureDiscovery.withProvider(
-        persistenceProvider: NoPersistenceProvider(),
-        child: Game(),
+      body: SafeArea(
+        child: FeatureDiscovery.withProvider(
+          persistenceProvider: NoPersistenceProvider(),
+          child: Game(),
+        ),
       ),
     );
   }
@@ -75,199 +77,186 @@ class _GameState extends State<Game> {
   Widget build(BuildContext context) {
     size = MediaQuery.of(context).size;
     double f = size.height / 470;
-    return WillPopScope(
-      onWillPop: () => Future.value(false),
-      child: Scaffold(
-        body: SafeArea(
-          child: InkWell(
-            onTap: () {
-              // startTimer = true;
-            },
-            child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/images/background-game.jpg'),
-                    fit: BoxFit.fill),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/background-game.jpg'),
+                fit: BoxFit.fill),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
+                  Expanded(
                     child: Container(
-                      // height: 100,
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Expanded(child: SizedBox()),
-                          DescribedFeatureOverlay(
-                            featureId: 'feature2',
-                            targetColor: Colors.white,
-                            textColor: Colors.white,
-                            backgroundColor: Theme.of(context).primaryColor,
-                            contentLocation: ContentLocation.trivial,
-                            title: Text(
-                              'On Tap Here',
-                              style: TextStyle(fontSize: 30.0),
-                            ),
-                            pulseDuration: Duration(seconds: 2),
-                            enablePulsingAnimation: true,
-                            overflowMode: OverflowMode.extendBackground,
-                            openDuration: Duration(seconds: 2),
-                            description: Text('Tap here repeatedly to earn points and move the food up to the plates'),
-                            tapTarget: CircularCountDownTimer(
-                              duration: 30,
-                              initialDuration: 0,
-                              controller: CountDownController(),
-                              width: MediaQuery.of(context).size.width / 7,
-                              height: MediaQuery.of(context).size.height / 7,
-                              ringColor: Colors.grey[300],
-                              ringGradient: null,
-                              fillColor: Color.fromRGBO(242,223,55, 0.90),
-                              fillGradient: null,
-                              backgroundColor: Color.fromRGBO(232,52,70, 0.90),
-                              backgroundGradient: null,
-                              strokeWidth: 8.0,
-                              strokeCap: StrokeCap.round,
-                              textStyle: TextStyle(
-                                  fontSize: 17.0,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            child: CircularCountDownTimer(
-                              duration: 30,
-                              initialDuration: 0,
-                              controller: CountDownController(),
-                              width: MediaQuery.of(context).size.width / 7,
-                              height: MediaQuery.of(context).size.height / 7,
-                              ringColor: Colors.grey[300],
-                              ringGradient: null,
-                              fillColor: Color.fromRGBO(242,223,55, 0.90),
-                              fillGradient: null,
-                              backgroundColor: Color.fromRGBO(232,52,70, 0.90),
-                              backgroundGradient: null,
-                              strokeWidth: 8.0,
-                              strokeCap: StrokeCap.round,
-                              textStyle: TextStyle(
-                                  fontSize: 17.0,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold),
-                              textFormat: CountdownTextFormat.S,
-                              isReverse: true,
-                              isReverseAnimation: false,
-                              isTimerTextShown: true,
-                              autoStart: true,
-                              onComplete: () {
-                                if (startTimer == true) {
-                                  insertScore();
-                                  end = true;
-                                }else {
-                                  //pass,
-                                }
-                              },
-                            ),
-                          ),
-                          Expanded(child: SizedBox()),
-                          Container(
-                              margin: EdgeInsets.only(
-                                top: (size.height / 2) - sum,
-                              ),
-                              child: Lottie.asset('assets/53947-sushi.json',
-                                  height: 170, width: 170)),
-                          Expanded(child: SizedBox()),
-                          Container(
-                            margin: EdgeInsets.only(
-                              top: 25,
-                            ),
-                            child: DescribedFeatureOverlay(
-                              featureId: 'feature3',
-                              targetColor: Colors.white,
-                              textColor: Colors.white,
-                              backgroundColor: Theme.of(context).primaryColor,
-                              contentLocation: ContentLocation.trivial,
-                              title: Text(
-                                'On Tap Here',
-                                style: TextStyle(fontSize: 30.0),
-                              ),
-                              pulseDuration: Duration(seconds: 2),
-                              enablePulsingAnimation: true,
-                              overflowMode: OverflowMode.extendBackground,
-                              openDuration: Duration(seconds: 2),
-                              description: Text('Tap here repeatedly to earn points and move the food up to the plates'),
-                              tapTarget: CircleAvatar(
-                                radius: 40.0,
-                                backgroundColor: Color.fromRGBO(37,100,138, 0.90),
-                                child: Text(
-                                  dSum.toString(),
-                                  style: TextStyle(
-                                      fontSize: 20, color: Colors.white),
-                                ),
-                              ),
-                              child: CircleAvatar(
-                                radius: 40.0,
-                                backgroundColor: Color.fromRGBO(37,100,138, 0.90),
-                                child: Text(
-                                  dSum.toString(),
-                                  style: TextStyle(
-                                      fontSize: 20, color: Colors.white),
-                                ),
-                              ),
-                            ),
-                          ),
-                          Expanded(child: SizedBox()),
-                        ],
+                      margin: EdgeInsets.only(top: MediaQuery.of(context).size.width / 9 ),
+                      child: DescribedFeatureOverlay(
+                        featureId: 'feature2',
+                        targetColor: Colors.white,
+                        textColor: Colors.white,
+                        backgroundColor: Theme.of(context).primaryColor,
+                        contentLocation: ContentLocation.below,
+                        title: Text(
+                          'Countdown timer',
+                          style: TextStyle(fontSize: 30.0),
+                        ),
+                        pulseDuration: Duration(seconds: 2),
+                        enablePulsingAnimation: true,
+                        overflowMode: OverflowMode.extendBackground,
+                        openDuration: Duration(seconds: 2),
+                        description: Text('In this section you can see \nthe remaining time of the game.',style: TextStyle(fontSize: 17.0),),
+                        tapTarget: CircularCountDownTimer(
+                          duration: 30,
+                          initialDuration: 0,
+                          controller: CountDownController(),
+                          width: MediaQuery.of(context).size.width / 5,
+                          height: MediaQuery.of(context).size.height / 5,
+                          ringColor: Colors.grey[300],
+                          ringGradient: null,
+                          fillColor: Color.fromRGBO(242,223,55, 0.90),
+                          fillGradient: null,
+                          backgroundColor: Color.fromRGBO(232,52,70, 0.90),
+                          backgroundGradient: null,
+                          strokeWidth: 8.0,
+                          strokeCap: StrokeCap.round,
+                          textStyle: TextStyle(
+                              fontSize: 17.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        child:
+                        startTimer == true
+                            ? CircularCountDownTimer(
+                          duration: 30,
+                          initialDuration: 0,
+                          controller: CountDownController(),
+                          width: 75,
+                          height: 75,
+                          ringColor: Colors.grey[300],
+                          ringGradient: null,
+                          fillColor: Color.fromRGBO(248, 92, 44, 0.99),
+                          fillGradient: null,
+                          backgroundColor: Color.fromRGBO(
+                              180, 6, 22, 0.90),
+                          backgroundGradient: null,
+                          strokeWidth: 8.0,
+                          strokeCap: StrokeCap.round,
+                          textStyle: TextStyle(
+                              fontSize: 17.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                          textFormat: CountdownTextFormat.S,
+                          isReverse: true,
+                          isReverseAnimation: false,
+                          isTimerTextShown: true,
+                          autoStart: true,
+                          onComplete: () {
+                              insertScore();
+                              end = true;
+                          },
+                        )
+                            : Expanded(child: SizedBox()),
                       ),
                     ),
                   ),
-                  Expanded(child: SizedBox()),
-                  DescribedFeatureOverlay(
-                    featureId: 'feature1',
-                    targetColor: Colors.white,
-                    textColor: Colors.white,
-                    backgroundColor: Theme.of(context).primaryColor,
-                    contentLocation: ContentLocation.trivial,
-                    title: Text(
-                      'On Tap Here',
-                      style: TextStyle(fontSize: 30.0),
-                    ),
-                    pulseDuration: Duration(seconds: 2),
-                    enablePulsingAnimation: true,
-                    overflowMode: OverflowMode.extendBackground,
-                    openDuration: Duration(seconds: 2),
-                    description: Text('Tap here repeatedly to earn points and move the food up to the plates'),
-                    tapTarget: Container(
-                      child: Lottie.asset(
-                          'assets/19611-ration-food-transition.json',
-                          height: 200,
-                          width: 200),
-                    ),
-                    child: CircleAvatar(
-                      radius: 50,
-                      child: InkWell(
-                        onTap: () {
-                          player.play();
-                          startTimer = true;
-                          if (!end) {
-                            dSum += 2;
-                            sum += f;
-                            setState(() {});
-                            print(sum);
-                          } else {
-                            insertScore();
-                          }
-                        },
-                        child: Container(
-                          child: Lottie.asset(
-                              'assets/19611-ration-food-transition.json',
-                              height: 200,
-                              width: 200),
+                  Container(
+                      margin: EdgeInsets.only(
+                        top: (size.height / 2) - sum,
+                      ),
+                      child: Lottie.asset('assets/53947-sushi.json',
+                          height: 170, width: 170)),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(top: MediaQuery.of(context).size.width / 9 ),
+                      child: DescribedFeatureOverlay(
+                        featureId: 'feature3',
+                        targetColor: Colors.white,
+                        textColor: Colors.white,
+                        backgroundColor: Theme.of(context).primaryColor,
+                        contentLocation: ContentLocation.below,
+                        title: Text(
+                          'Points taken',
+                          style: TextStyle(fontSize: 30.0),
+                        ),
+                        pulseDuration: Duration(seconds: 2),
+                        enablePulsingAnimation: true,
+                        overflowMode: OverflowMode.extendBackground,
+                        openDuration: Duration(seconds: 2),
+                        description: Text('In this section, you can see the points collected in the game.',style: TextStyle(fontSize: 17.0)),
+                        tapTarget: CircleAvatar(
+                          radius: 40.0,
+                          backgroundColor: Color.fromRGBO(37,100,138, 0.99),
+                          child: Text(
+                            dSum.toString(),
+                            style: TextStyle(
+                                fontSize: 20, color: Colors.white),
+                          ),
+                        ),
+                        child: CircleAvatar(
+                          radius: 40.0,
+                          backgroundColor: Color.fromRGBO(37,100,138, 0.99),
+                          child: Text(
+                            dSum.toString(),
+                            style: TextStyle(
+                                fontSize: 20, color: Colors.white),
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ],
               ),
-            ),
+              Expanded(child: SizedBox()),
+              DescribedFeatureOverlay(
+                featureId: 'feature1',
+                targetColor: Colors.white,
+                textColor: Colors.white,
+                backgroundColor: Theme.of(context).primaryColor,
+                contentLocation: ContentLocation.above,
+                title: Text(
+                  'On Tap Here',
+                  style: TextStyle(fontSize: 30.0),
+                ),
+                pulseDuration: Duration(seconds: 2),
+                enablePulsingAnimation: true,
+                overflowMode: OverflowMode.extendBackground,
+                openDuration: Duration(seconds: 2),
+                description: Text('Tap here repeatedly to earn points and move the food up to the plates',style: TextStyle(fontSize: 17.0)),
+                tapTarget: Container(
+                  child: Lottie.asset(
+                      'assets/19611-ration-food-transition.json',
+                      height: 200,
+                      width: 200),
+                ),
+                child: CircleAvatar(
+                  radius: 50,
+                  child: InkWell(
+                    onTap: () {
+                      player.play();
+                      startTimer = true;
+                      if (!end) {
+                        dSum += 2;
+                        sum += f;
+                        setState(() {});
+                        print(sum);
+                      } else {
+                        insertScore();
+                      }
+                    },
+                    child: Container(
+                      child: Lottie.asset(
+                          'assets/19611-ration-food-transition.json',
+                          height: 200,
+                          width: 200),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
